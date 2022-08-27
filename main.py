8e7bd025357e276f9586b98f7942d37a130b7d05
Uday Rana <82779467+uday-rana@users.noreply.github.com>
2022-08-27 16:29:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+# This code is based on the following example:
+# https://discordpy.readthedocs.io/en/stable/quickstart.html#a-minimal-bot
+
+import discord
+import os
+import json
+import requests
+from keep_alive import keep_alive
+
+
+def seinfeld():
+    response = requests.get("https://seinfeld-quotes.herokuapp.com/random")
+    data = json.loads(response.text)
+    quote = "\"" + data["quote"] + "\"" + ' -' + data[
+        "author"] + ", Season " + data["season"] + ", Episode " + data[
+            "episode"]
+    return (quote)
+
+
+my_secret = os.environ['TOKEN']
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$seinfeld'):
+        quote = seinfeld()
+        await message.channel.send(quote)
+
+
+try:
+    keep_alive()
+    client.run(os.getenv("TOKEN"))
+except discord.HTTPException as e:
+    if e.status == 429:
+        print(
+            "The Discord servers denied the connection for making too many requests"
+        )
+        print(
+            "Get help from https://stackoverflow.com/questions/66724687/in-discord-py-how-to-solve-the-error-for-toomanyrequests"
+        )
+    else:
+        raise e
