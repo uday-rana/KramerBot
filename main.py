fc06a6cf9af09fe5010c1060f3fc7c0941c52af9
Dragos Penciu <108203113+quaggantheblue@users.noreply.github.com>
2022-08-28 15:09:26 +0000

    code rework + guess function

--- main.py
+++ main.py
@@ -2,48 +2,66 @@
 # https://discordpy.readthedocs.io/en/stable/quickstart.html#a-minimal-bot
 
 import discord
+from discord.ext import commands
 import os
 import json
 import requests
 from keep_alive import keep_alive
+import random
 
-def seinfeld(guess=False):
+my_secret = os.environ['TOKEN']
+bot = commands.Bot(command_prefix='$')
+seinfeld = []
+
+def make_quote(guess):
     response = requests.get("https://seinfeld-quotes.herokuapp.com/random")
     data = json.loads(response.text)
 
     quote = "```" + data["quote"] + "```"
     if guess == False:
-        quote = quote + ' -' + data["author"]
+      quote = quote + ' -' + data["author"]
     quote = quote + " (Season " + data["season"] + ", Episode " + data[
         "episode"] + ')'
-    return (quote)
+    return [quote, data["author"]]
 
-my_secret = os.environ['TOKEN']
-client = discord.Client()
+def get_author():
+    response = requests.get("https://seinfeld-quotes.herokuapp.com/random")
+    data = json.loads(response.text)
+    return data["author"]
 
+def get_options(answer):
+  author1 = get_author()
+  author2 = get_author()
+  list = [author1, author2, answer]
+  random.shuffle(list)
+  return list
 
-@client.event
+@bot.command()
+async def seinfeld(ctx):
+    guess = False
+    quote = make_quote(guess)
+    await ctx.send(quote[0])
+
+@bot.command()
+async def guess(ctx):
+    quote = make_quote(guess = True)
+    options = get_options(quote[1])
+    print(options)
+    await ctx.send(quote[0])
+    prompt = await ctx.send(
+      f"Who said this quote?\n🇦: {options[0]}\n🇧: {options[1]}\n🇨: {options[2]}"
+    )
+    await prompt.add_reaction('🇦')
+    await prompt.add_reaction('🇧')
+    await prompt.add_reaction('🇨')
+
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$seinfeld'):
-        quote = seinfeld()
-        await message.channel.send(quote)
-
-    if message.content.startswith('$guess'):
-        quote = seinfeld(True)
-        await message.channel.send(quote)
-
+    print('We have logged in as {0.user}'.format(bot))
 
 try:
     keep_alive()
-    client.run(os.getenv("TOKEN"))
+    bot.run(os.getenv("TOKEN"))
 except discord.HTTPException as e:
     if e.status == 429:
         print(
